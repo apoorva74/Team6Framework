fc5cdeeb993e0b3e2196da9640068b83358c20f7
Apoorva Stephen <apoorva.stephen5@gmail.com>
2018-04-28 09:45:07 -0400

    final commit

--- BestBuy_Retail/src/main/java/home/MainBestBuy.java
+++ BestBuy_Retail/src/main/java/home/MainBestBuy.java
@@ -6,26 +6,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
 public class MainBestBuy extends CommonAPIApoorva {
-    static WebDriverWait wait = new WebDriverWait (Driver, 1,500);
+    static WebDriverWait wait = new WebDriverWait(Driver, 1, 500);
 
-    public static void closePopUp(){
+    public static void closePopUp() {
 
         WebElement closeButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".email-submission-modal .modal-header button.close")));
         closeButton.click();
     }
 
-    public static void scrollDown(){
+    public static void scrollDown() {
         JavascriptExecutor js = (JavascriptExecutor) Driver;
         js.executeScript("window.scrollBy(0,500)");
     }
 
-    public static void scrollUp(){
+    public static void scrollUp() {
         JavascriptExecutor js = (JavascriptExecutor) Driver;
         js.executeScript("window.scrollBy(0,-500)");
     }
@@ -46,7 +47,8 @@ public class MainBestBuy extends CommonAPIApoorva {
 
         account.click(); //now it clicks on element
     }
-    public static void accountButton(){
+
+    public static void accountButton() {
 
         WebElement account = Driver.findElement(By.cssSelector("a.am-create-account__button"));
         wait.until(ExpectedConditions.visibilityOf(account)); //this will wait for elememt to be visible for 20 seconds
@@ -67,12 +69,13 @@ public class MainBestBuy extends CommonAPIApoorva {
         Thread.sleep(2000);
         Driver.findElement(By.cssSelector("#fld-phone")).sendKeys("123456789");
         Thread.sleep(2000);
-       createAccount = Driver.findElement(By.cssSelector(".cia-form__submit-button"));
+        createAccount = Driver.findElement(By.cssSelector(".cia-form__submit-button"));
         wait.until(ExpectedConditions.visibilityOf(createAccount)); //this will wait for elememt to be visible for 20 seconds
         createAccount.click();
 
     }
-    public static void signInButton(){
+
+    public static void signInButton() {
 
         WebElement signInB = null;
         signInB = Driver.findElement(By.cssSelector(".lam-signIn__button"));
@@ -93,20 +96,83 @@ public class MainBestBuy extends CommonAPIApoorva {
 
     public static void featuredItems() {
         String title = Driver.findElement(By.cssSelector(".featured > h2:nth-child(1)")).getText();
-        System.out.println("The "+ title + " items are:");
+        System.out.println("The " + title + " items are:");
 
+        /*
         for(WebElement link: Driver.findElements(By.xpath("//div[@class=\"featured\"]//a")))
         {
             System.out.println(link.getText());
-        }
+            sleep();
+            link.click();
+            sleep();
+            Driver.findElement(By.cssSelector("a#menu0")).click();
+        } */
 
-        }
+
+        List <WebElement> alllinks = Driver.findElements(By.xpath("//div[@class=\"featured\"]//a"));
+        for (int i = 0; i < alllinks.size(); i++)
+            System.out.println(alllinks.get(i).getText());
+
+        //  for (int i = 0; i < alllinks.size(); i++) {
+        //Driver.navigate().to(String.valueOf(alllinks.get(i)));
+        // alllinks.get(i).click();
+        // Driver.navigate().back();
+        // sleep();
+        // Driver.findElement(By.cssSelector("a#menu0")).click();
+        // Driver.findElements(By.xpath("//div[@class=\"featured\"]//a"));
+
+
     }
 
 
-   /* public static void scrollMostViewed(){
-        Point item =Driver.findElement(By.xpath('//*[@id="widget-be0c78f8-cc60-4414-aa27-058521107900"]/div/div[2]/div/div/div[2]/a[2]/div')).getLocation();
-        ((JavascriptExecutor)Driver).executeScript("window.scrollBy(0,"+(item.getY())+");");
-        actions.moveToElement(right).click().build().perform();
+    public static void allCategories() {
+        String title = Driver.findElement(By.cssSelector(".shopby > h2:nth-child(1)")).getText();
+        System.out.println("The " + title + " items are:");
+        List <WebElement> shopby = Driver.findElements(By.xpath("//div[@class=\"shopby\"]//li[@class=\"js-navitem sub-nav-parent taphover\"]"));
+        for (int i = 0; i < shopby.size(); i++)
+            System.out.println(shopby.get(i).getText());
+
     }
-*/
+
+    public static void featuredOffers() {
+        String title = Driver.findElement(By.cssSelector("#widget-a9596c6c-a001-4460-ae39-bf48cdcf4822 > div > p")).getText();
+        System.out.println("The " + title + " items are:");
+
+        List <WebElement> heading = Driver.findElements(By.xpath("//h2[@class=\"header-minimal\"]"));
+        Actions actions = new Actions(Driver);
+        JavascriptExecutor js = (JavascriptExecutor) Driver;
+        js.executeScript("window.scrollBy(0,500)");
+        List <WebElement> alllinks = Driver.findElements(By.xpath("//div[@class=\"image-shell\"]//a"));
+        for (int i = 0; i < heading.size(); i++)
+            System.out.println(heading.get(i).getText());
+
+        for (int i = 0; i < alllinks.size(); i++) {
+            js.executeScript("window.scrollBy(0,607)");
+            sleep();
+            actions.moveToElement(alllinks.get(i)).click().build().perform();
+            // alllinks.get(i).click();
+            Driver.navigate().back();
+            sleep();
+        }
+
+    }
+
+    public static void myBestBuy() {
+        String title = Driver.findElement(By.cssSelector("//*[@id=\"footer\"]/div[2]/div[1]/div[1]/div[1]/a")).getText();
+        System.out.println("The " + title + " items are:");
+        List <WebElement> alllinks = Driver.findElements(By.xpath("//*[@id=\"footer\"]/div[2]/div[1]/div[1]/div[1]/ul//a"));
+        for (int i = 0; i < alllinks.size(); i++)
+            System.out.println(alllinks.get(i).getText());
+
+    }
+
+    public static void creditCard() {
+        String title = Driver.findElement(By.cssSelector("//*[@id=\"footer\"]/div[2]/div[1]/div[1]/div[2]/a")).getText();
+        System.out.println("The " + title + " items are:");
+        List <WebElement> alllinks = Driver.findElements(By.xpath("//*[@id=\"footer\"]/div[2]/div[1]/div[1]/div[2]/ul//a"));
+        for (int i = 0; i < alllinks.size(); i++)
+            System.out.println(alllinks.get(i).getText());
+    }
+}
+
+
--- BestBuy_Retail/src/test/java/bestbuyTests/BestBuy.java
+++ BestBuy_Retail/src/test/java/bestbuyTests/BestBuy.java
@@ -4,7 +4,9 @@ import commonAPI.CommonAPIApoorva;
 import home.MainBestBuy;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.openqa.selenium.support.ui.Select;
@@ -65,10 +67,11 @@ public class BestBuy extends CommonAPIApoorva {
 public void testAccount() throws InterruptedException {
             Driver.get("https://bestbuy.com");
             home.MainBestBuy.closePopUp();
+            //Test 6 - open account
             home.MainBestBuy.account();
-            //Test 6 - go to create Account
+            //Test 7 - navigate to create Account
             home.MainBestBuy.accountButton();
-            //Test 7 - create account
+            //Test 8 - create account
              home.MainBestBuy.createAccount();
     }
 
@@ -77,9 +80,9 @@ public void testAccount() throws InterruptedException {
         Driver.get("https://bestbuy.com");
         home.MainBestBuy.closePopUp();
         home.MainBestBuy.account();
-        //Test 8 - go to sign in page
+        //Test 9 - go to sign in page
         home.MainBestBuy.signInButton();
-        //Test 9 - sign in
+        //Test 10 - sign in
         home.MainBestBuy.signIn();
     }
 
@@ -88,19 +91,121 @@ public void testAccount() throws InterruptedException {
 
         Driver.get("https://bestbuy.com");
         home.MainBestBuy.closePopUp();
-        //Test 10 - go to product
+        //Test 11 - go to product
         home.MainBestBuy.sleep();
         Driver.findElement(By.cssSelector("a#menu0")).click();
         home.MainBestBuy.sleep();
-        //Test 11 - close product
+        //Test 12 - close product
         Driver.findElement(By.xpath("//*[@id='group0']/div[3]/a")).click();
         //open products again
         Driver.findElement(By.cssSelector("a#menu0")).click();
-        //Test 12 - print all featured items
+        //Test 13 - print all featured items
         home.MainBestBuy.featuredItems();
-
+        //Test 14 - print all categories items
+        home.MainBestBuy.allCategories();
+        //Test 15 - print featured offer
+        //Test 16 - navigate to featured offers
+        home.MainBestBuy.featuredOffers();
     }
 
+    //---------------------------------
+
+    @Test //test3
+    public void Searchbar(){
+       // Actions actions = new Actions(Driver);
+        Driver.get("https://bestbuy.com");
+        home.MainBestBuy.closePopUp();
+        //Test 17 - locate search bar
+        Driver.findElement(By.xpath("//*[@id=\"gh-search-input\"]"));
+        //Test 18 - enter value to search bar
+        Driver.findElement(By.xpath("//*[@id=\"gh-search-input\"]")).sendKeys("laptop",Keys.ENTER);
+        home.MainBestBuy.sleep();
+        //Test 19 - clear search
+        Driver.findElement(By.xpath("//*[@id=\"header\"]/div[1]/div[2]/div[2]/form/button[1]")).click();
+        Driver.findElement(By.xpath("//*[@id=\"gh-search-input\"]")).sendKeys("laptop",Keys.ENTER);
+        home.MainBestBuy.sleep();
+        //Test 20 - Search laptop
+        Driver.findElement(By.xpath("//*[@id=\"header\"]/div[1]/div[2]/div[2]/form/button[2]")).click();
+
+        home.MainBestBuy.sleep();
+       //Driver.findElements(By.xpath("/html/body/header/div[1]/div[2]/div[2]/form/button[2]"));
+       // actions.moveToElement(By.cssSelector("/#header > div:nth-child(2) > div.primary-wrap > div.search-bar > form > button.header-search-button")).click().build().perform();
+        //Test 21 - navigate back to home Screen
+        Driver.navigate().back();
+        //Test 22 - refresh page
+        Driver.navigate().refresh();
+        //Test 23 -navigate forward
+        Driver.navigate().forward();
+        // Test 24 - navigate forward and clear search
+        Driver.findElement(By.xpath("//*[@id=\"header\"]/div[1]/div[2]/div[2]/form/button[1]")).click();
+        Driver.navigate().refresh();
+
+        }
+@Test
+    public void myBestBuy() {
+    Driver.get("https://www.bestbuy.com");
+    home.MainBestBuy.closePopUp();
+    //Test 25 - navigate to My best buy page
+    Driver.findElement(By.xpath("//*[@id=\"footer\"]/div[2]/div[1]/div[1]/div[1]/a")).click();
+    home.MainBestBuy.sleep();
+    //Test 26 - go back to best buy page
+    Driver.navigate().back();
+    home.MainBestBuy.sleep();
+    //Test 27 print all my best buy options
+    home.MainBestBuy.myBestBuy();
+    //Test 28 - navigate to view points and certificate page
+    Driver.findElement(By.xpath("//*[@id=\"footer\"]/div[2]/div[1]/div[1]/div[1]/ul/li[1]/a")).click();
+    //Test 29 - go back to best buy page
+    Driver.navigate().back();
+    home.MainBestBuy.sleep();
+    //Test 30 - navigate to view Member offer page
+    Driver.findElement(By.xpath("//*[@id=\"footer\"]/div[2]/div[1]/div[1]/div[1]/ul/li[2]/a")).click();
+    //Test 31 - go back to best buy page
+    Driver.navigate().back();
+    home.MainBestBuy.sleep();
+    //Test 32 - navigate to sign in or create account page
+    Driver.findElement(By.xpath("//*[@id=\"footer\"]/div[2]/div[1]/div[1]/div[1]/ul/li[3]/a")).click();
+    //Test 33 - go back to best buy page
+    Driver.navigate().back();
+    home.MainBestBuy.sleep();
+
+}
+@Test
+
+public void creditCard(){
+    Driver.get("https://www.bestbuy.com");
+    home.MainBestBuy.closePopUp();
+    //Test 34 - print all credit card options
+    home.MainBestBuy.creditCard();
+    //Test 35 - navigate to credit card page
+    Driver.findElement(By.xpath("//*[@id=\"footer\"]/div[2]/div[1]/div[1]/div[2]/a")).click();
+    //Test 36 - go back to best buy page
+    Driver.navigate().back();
+    home.MainBestBuy.sleep();
+    //Test 37 - navigate to make payment page
+    Driver.findElement(By.xpath("//*[@id=\"footer\"]/div[2]/div[1]/div[1]/div[2]/ul/li[1]/a")).click();
+    //Test 38 - go back to best buy page
+    Driver.navigate().back();
+    home.MainBestBuy.sleep();
+    //Test 39 - navigate to credit card offers page
+    Driver.findElement(By.xpath("//*[@id=\"footer\"]/div[2]/div[1]/div[1]/div[2]/ul/li[2]/a")).click();
+    //Test 40 - go back to best buy page
+    Driver.navigate().back();
+    home.MainBestBuy.sleep();
+    //Test 41 - navigate to apply now page
+    Driver.findElement(By.xpath("//*[@id=\"footer\"]/div[2]/div[1]/div[1]/div[2]/ul/li[3]/a")).click();
+    //Test 42 - go back to best buy page
+    Driver.navigate().back();
+    home.MainBestBuy.sleep();
+}
+
+@AfterMethod
+    public static void  close(){
+
+        Driver.get("https://www.Bestbuy.com");
+        Driver.close();
+
+}
 }
 
 
